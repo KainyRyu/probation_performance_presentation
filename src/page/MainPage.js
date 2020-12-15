61b322ddb70d78ddb923a19f76c67d44e759e358
KainyRyu <kainy2109@gmail.com>
2020-12-15 23:46:13 +0900

    :lipstick: Create MainPage
    Create MainPage component

---
+++ src/page/MainPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+export default function MainPage() {
+  return (
+    <div>
+      {/* Header */}
+      <Contexts>
+        <h1>조아라에서 3개월... </h1>
+      </Contexts>
+    </div>
+  );
+}
+
+const Contexts = styled.div`
+  color: #333333;
+`;
