9c522077668d673f758f36c5b313798b980db26f
KainyRyu <kainy2109@gmail.com>
2020-12-20 14:34:53 +0900

    :triangular_flag_on_post: Display d-day to the presentation date
    Display d-day to the presentation date
    
      Committer: KainyRyu <kainy2109@gmail.com>

--- src/component/Timer.js
+++ src/component/Timer.js
@@ -1,37 +1,70 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState } from 'react';
 import styled from '@emotion/styled';
 
 export default function Timer() {
-  // const [currentTime, setCurrentTime] = useState();
-  const newDate = new Date();
-  const hour = newDate.getHours();
-  const minutes = newDate.getMinutes();
-  const seconds = newDate.getSeconds();
-  const milliseconds = newDate.getMilliseconds();
+  const [dueDay, setDueDay] = useState();
+  const [dueTime, setDueTime] = useState();
 
-  const currentTime = useMemo(
-    () => `${hour}${minutes}${seconds}${milliseconds}`,
-    []
-  );
+  setInterval(function () {
+    let countDownDate = new Date('Feb 8, 2021 14:00:00').getTime();
+    let now = new Date().getTime();
+    let timeleft = countDownDate - now;
 
-  console.log(currentTime);
-  console.log(hour);
-  // console.log(newDate.getMilliseconds());
+    const days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(
+      (timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+
+    setDueDay(days);
+    setDueTime(` ${hours}: ${minutes}: ${seconds}`);
+  }, 1000);
 
   return (
     <div>
       <TimeBar>
         <Overlay />
       </TimeBar>
+      <StartBtn>START!</StartBtn>
+      <TimerBorder>
+        <TimeLeft>{dueDay}days</TimeLeft>
+        <TimeLeft>{dueTime}</TimeLeft>
+      </TimerBorder>
     </div>
   );
 }
 
 const TimeBar = styled.div`
   border: solid 1px black;
+  height: 30px;
   width: 30vw;
 `;
 
 const Overlay = styled.div`
-  display: block;
+  background: dodgerblue;
+  position: absolute;
+  height: 20px;
+  width: 20px;
+`;
+
+const StartBtn = styled.button`
+  background: white;
+  border: black 1px solid;
+`;
+
+const TimerBorder = styled.span`
+  align-items: center;
+  border: 2px red solid;
+  border-radius: 200px;
+  display: flex;
+  flex-flow: column;
+  font-size: 50px;
+  justify-content: center;
+  height: 300px;
+  width: 300px;
+`;
+
+const TimeLeft = styled.div`
+  color: red;
 `;
